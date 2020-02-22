c2fe308dadb8e141d082f65ba5cafd1b8db4dd2b
Anton B <>
2020-02-21 20:35:33 -0500

    part 5 - ViewModel

--- app/src/main/java/com/uwaterloo/watodo/MainActivity.java
+++ app/src/main/java/com/uwaterloo/watodo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.uwaterloo.watodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update RecyclerView
+                Toast.makeText(MainActivity.this, "onChange", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
